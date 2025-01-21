386cfb49bfeb94f2f3f3256a9002a5a5f5e7ab41
Vincent von Nordeck <vincent.nordeck@gmail.com>
2025-01-21 17:23:42 +0100

    Implemented the create-feature for islands

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -6,4 +6,25 @@ class IslandsController < ApplicationController
   def show
     @island = Island.find(params[:id])
   end
+
+  def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+    @island.user = current_user
+    if @island.save!
+      redirect_to island_path(@island), notice: "Island was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :description, :location, :longitude, :latitute, :price, :capacity)
+  end
+
 end
